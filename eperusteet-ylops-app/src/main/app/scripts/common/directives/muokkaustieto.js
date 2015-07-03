f5e2b627f6150c16b1ff931670e6c2c1f4963d81
Niko Junkala <niko.junkala@gofore.com>
2015-07-03 10:35:07 +0300

    Lisätty tekstikappaleen esitykseen muokkaustiedot

---
+++ eperusteet-ylops-app/src/main/app/scripts/common/directives/muokkaustieto.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software: Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * European Union Public Licence for more details.
+ */
+
+'use strict';
+
+angular.module('ylopsApp')
+  .directive('muokkaustieto', function($rootScope, EperusteetKayttajatiedot) {
+    return {
+      template:
+        '<div class="dir-muokkaustieto">' +
+        '  <span>' +
+        '    <span class="muokkaustieto-topic" kaanna="muokattu-viimeksi"></span>:' +
+        '    <span class="muokkaustieto-data" ng-bind="(tiedot.muokattu || tiedot.luotu) | aikaleima"></span>' +
+        '  </span>' +
+        '  <span style="margin-left: 5px" ng-if="muokkaajanNimi">' +
+        '    <span class="muokkaustieto-topic" kaanna="muokkaaja"></span>:' +
+        '    <span class="muokkaustieto-data" ng-bind="muokkaajanNimi"></span>' +
+        '  </span>' +
+        '</div>',
+      scope: {
+        tiedot: '='
+      },
+      link: function() {
+      },
+      controller: function($scope) {
+        EperusteetKayttajatiedot.get({
+          oid: $scope.tiedot.muokkaajaOid
+        }, function(res) {
+          if (res.sukunimi && (res.kutsumanimi || res.etunimet)) {
+            $scope.muokkaajanNimi = (res.kutsumanimi || res.etunimet) + ' ' + res.sukunimi;
+          }
+        });
+      }
+    };
+  });
