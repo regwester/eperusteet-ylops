3376d7aa88c363097e7441c680c5f555e6e8d62f
Niko Junkala <niko.junkala@gofore.com>
2015-10-19 20:04:40 +0300

    Korjaa #515

--- eperusteet-ylops-app/src/main/app/scripts/views/opetussuunnitelmat/vuosiluokat/vlk.js
+++ eperusteet-ylops-app/src/main/app/scripts/views/opetussuunnitelmat/vuosiluokat/vlk.js
@@ -173,12 +173,11 @@ ylopsApp
 
 
   // Lokalisaatioavaimet ohjepopover:ien sisällöille
-  var vlkNimi = vlk.nimi.fi;
-  $scope.vlkErityispiirteet = vlkNimi + '_erityispiirteet_ja_tehtavat_info';
+  $scope.vlkErityispiirteet = 'vuosiluokkakokonaisuuden-erityispiirteet-ja-tehtavat-info';
   $scope.siirtymaInfot = {};
-  $scope.siirtymaInfot.siirtymaEdellisesta = vlkNimi + '_siirtyma_aikaisempi_nykyinen_info';
-  $scope.siirtymaInfot.siirtymaSeuraavaan = vlkNimi + '_siirtymä_nykyinen_seuraava_info';
-  $scope.vlkLaajaalaiset = vlkNimi + '_laaja-alaisen_osaamisen_alueet_info';
+  $scope.siirtymaInfot.siirtymaEdellisesta = 'vuosiluokkakokonaisuuden-siirtyma-aikaisempi-nykyinen-info';
+  $scope.siirtymaInfot.siirtymaSeuraavaan = 'vuosiluokkakokonaisuuden-siirtymä-nykyinen-seuraava-info';
+  $scope.vlkLaajaalaiset = 'vuosiluokkakokonaisuuden-laaja-alaisen-osaamisen-alueet-info';
 
   var laajaalaisetosaamiset = _.indexBy(baseLaajaalaiset, 'tunniste');
   var laajaalaisetOrder = _(baseLaajaalaiset).sortBy(Utils.sort).map('tunniste').value();
