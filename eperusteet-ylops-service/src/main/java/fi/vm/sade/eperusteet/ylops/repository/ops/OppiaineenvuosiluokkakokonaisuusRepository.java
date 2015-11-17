6785b74027c0b6b29b98262a718d0641f51c4ca2
Niko Junkala <niko.junkala@gofore.com>
2015-11-17 20:57:15 +0200

    Korjattu editointikontrollit ja tekstikappaleiden editointi

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/repository/ops/OppiaineenvuosiluokkakokonaisuusRepository.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/repository/ops/OppiaineenvuosiluokkakokonaisuusRepository.java
@@ -29,4 +29,6 @@ public interface OppiaineenvuosiluokkakokonaisuusRepository extends JpaWithVersi
 
     @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END FROM Oppiaine o JOIN o.vuosiluokkakokonaisuudet vk WHERE o.id = ?1 AND vk.id = ?2)")
     boolean exists(long oppiaineId, long kokonaisuusId);
+
+
 }
