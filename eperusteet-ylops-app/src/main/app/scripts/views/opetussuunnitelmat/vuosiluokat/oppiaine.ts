3b79f374336a2fbaebb6248ea780a57972d45341
Niko Junkala <niko.junkala@gofore.com>
2015-12-07 19:04:09 +0200

    Korjaus vuosiluokkaistamiseen

--- eperusteet-ylops-app/src/main/app/scripts/views/opetussuunnitelmat/vuosiluokat/oppiaine.ts
+++ eperusteet-ylops-app/src/main/app/scripts/views/opetussuunnitelmat/vuosiluokat/oppiaine.ts
@@ -205,10 +205,6 @@ ylopsApp
       });
       vlk.$sisaltoalueetShort = allShort;
     });
-
-    if (!$scope.oppiaineenVlk.yleistavoitteet) {
-      VuosiluokkakokonaisuusMapper.createEmptyText($scope.oppiaineenVlk, 'yleistavoitteet');
-    }
   }
   updateVuosiluokat();
 
