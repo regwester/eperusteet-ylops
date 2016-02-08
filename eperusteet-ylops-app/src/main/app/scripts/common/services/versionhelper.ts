46ee9fb38c4990ae611afa7f52f3461062f1ec9f
Niko Junkala <niko.junkala@gofore.com>
2016-02-08 14:26:53 +0200

    X

--- eperusteet-ylops-app/src/main/app/scripts/common/services/versionhelper.ts
+++ eperusteet-ylops-app/src/main/app/scripts/common/services/versionhelper.ts
@@ -38,14 +38,7 @@ ylopsApp
 
     })
 
-<<<<<<< b546c794d83d66c5188052814fadfc923f1b5e6f:eperusteet-ylops-app/src/main/app/scripts/common/services/versionhelper.ts
     .controller('HistoryViewController', ($scope, versions, $modalInstance) => {
-||||||| merged common ancestors
-    .controller('HistoryViewCtrl', ($scope, versions, $modalInstance) => {
-=======
-    .controller('HistoryViewController', ($scope, versions, $modalInstance) => {
-        console.log('initing');
->>>>>>> Vaihdettu tiedostonimi:eperusteet-ylops-app/src/main/app/scripts/common/services/versionhelper.ts
         $scope.versions = versions;
         $scope.close = (versio, current) => {
             if (versio) {
