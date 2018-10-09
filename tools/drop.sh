d34daabbac1f83232af0c4262c518f70d453e5f3
Sauli Anto <sauli.anto@gmail.com>
2018-10-09 13:44:21 +0300

    Siistitty hakemistoja

--- tools/drop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-psql -U postgres -c "DROP DATABASE ylopstest;"
-psql -U postgres -c "CREATE DATABASE ylopstest;"
-psql -U postgres -c "CREATE USER ylopstest WITH PASSWORD 'ylopstest';"
-psql -U postgres -c "GRANT ALL ON DATABASE ylopstest TO ylopstest;"
