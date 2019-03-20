ca1824365cafe587634e75c97933ffc72b3e2b18
Sauli Anto <sauli.anto@gmail.com>
2019-03-20 13:29:37 +0200

    Korjattu migraatio ja poistettu kahteen kertaan määritelty plugini

--- eperusteet-ylops-service/src/main/resources/db/migration/V1_84__lisatty_kysymys_taulu.sql
+++ eperusteet-ylops-service/src/main/resources/db/migration/V1_84__lisatty_kysymys_taulu.sql
@@ -1,4 +1,8 @@
-create table Kysymys (
+drop table if exists kysymys cascade;
+drop table if exists kysymys_aud cascade;
+drop table if exists kysymys_organisaatiot cascade;
+
+create table kysymys (
     id int8 not null,
     luoja varchar(255),
     luotu timestamp,
@@ -9,21 +13,21 @@ create table Kysymys (
     primary key (id)
 );
 
-create table Kysymys_AUD (
+create table kysymys_aud (
     id int8 not null,
-    REV int4 not null,
-    REVTYPE int2,
-    REVEND int4,
+    rev int4 not null,
+    revtype int2,
+    revend int4,
     luoja varchar(255),
     luotu timestamp,
     muokattu timestamp,
     muokkaaja varchar(255),
     kysymys_id int8,
     vastaus_id int8,
-    primary key (id, REV)
+    primary key (id, rev)
 );
 
-create table Kysymys_organisaatiot (
-    Kysymys_id int8 not null,
+create table kysymys_organisaatiot (
+    kysymys_id int8 not null,
     organisaatiot varchar(255)
 );
