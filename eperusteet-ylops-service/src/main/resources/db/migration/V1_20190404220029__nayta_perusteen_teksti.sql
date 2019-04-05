a7e25ac39851b38e217a3d9d18d2b1cf2a694755
Niko Junkala <niko.junkala@gofore.com>
2019-04-05 11:07:20 +0300

    Korjattu migraatio

--- eperusteet-ylops-service/src/main/resources/db/migration/V1_20190404220029__nayta_perusteen_teksti.sql
+++ eperusteet-ylops-service/src/main/resources/db/migration/V1_20190404220029__nayta_perusteen_teksti.sql
@@ -1,5 +1,5 @@
-ALTER TABLE tekstikappaleviite ADD COLUMN nayta_perusteen_teksti BOOLEAN NOT NULL;
-ALTER TABLE tekstikappaleviite_aud ADD COLUMN nayta_perusteen_teksti BOOLEAN NOT NULL;
+ALTER TABLE tekstikappaleviite ADD COLUMN nayta_perusteen_teksti BOOLEAN DEFAULT TRUE;
+ALTER TABLE tekstikappaleviite_aud ADD COLUMN nayta_perusteen_teksti BOOLEAN DEFAULT TRUE;
 
-ALTER TABLE lops2019_opintojakso ADD COLUMN laajuus int8 not null default 0;
-ALTER TABLE lops2019_opintojakso_aud ADD COLUMN laajuus int8 not null default 0;
+ALTER TABLE lops2019_opintojakso ADD COLUMN laajuus int8 default 0;
+ALTER TABLE lops2019_opintojakso_aud ADD COLUMN laajuus int8 default 0;
