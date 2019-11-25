c8fc5a121f639b54871f67d081ddb6ac423ee607
Sauli Anto <sauli.anto@gmail.com>
2019-11-25 15:44:01 +0200

    Lisätty oppimäärät ja opintojaksot navigaatioon

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/ops/impl/NavigationBuilderLops2019Impl.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/ops/impl/NavigationBuilderLops2019Impl.java
@@ -3,15 +3,18 @@ package fi.vm.sade.eperusteet.ylops.service.ops.impl;
 import com.google.common.collect.Sets;
 import fi.vm.sade.eperusteet.utils.dto.peruste.lops2019.tutkinnonrakenne.KoodiDto;
 import fi.vm.sade.eperusteet.ylops.domain.KoulutustyyppiToteutus;
-import fi.vm.sade.eperusteet.ylops.domain.lops2019.Lops2019Sisalto;
+import fi.vm.sade.eperusteet.ylops.dto.lops2019.Lops2019OpintojaksoDto;
+import fi.vm.sade.eperusteet.ylops.dto.lops2019.Lops2019OpintojaksonOppiaineDto;
 import fi.vm.sade.eperusteet.ylops.dto.lops2019.Lops2019PaikallinenOppiaineDto;
 import fi.vm.sade.eperusteet.ylops.dto.navigation.NavigationNodeDto;
 import fi.vm.sade.eperusteet.ylops.dto.navigation.NavigationType;
 import fi.vm.sade.eperusteet.ylops.dto.peruste.lops2019.oppiaineet.Lops2019OppiaineKaikkiDto;
+import fi.vm.sade.eperusteet.ylops.dto.peruste.lops2019.oppiaineet.moduuli.Lops2019ModuuliDto;
 import fi.vm.sade.eperusteet.ylops.dto.teksti.LokalisoituTekstiDto;
 import fi.vm.sade.eperusteet.ylops.repository.lops2019.Lops2019SisaltoRepository;
 import fi.vm.sade.eperusteet.ylops.repository.ops.OpetussuunnitelmaRepository;
 import fi.vm.sade.eperusteet.ylops.service.external.EperusteetService;
+import fi.vm.sade.eperusteet.ylops.service.lops2019.Lops2019OpintojaksoService;
 import fi.vm.sade.eperusteet.ylops.service.lops2019.Lops2019OppiaineService;
 import fi.vm.sade.eperusteet.ylops.service.lops2019.Lops2019Service;
 import fi.vm.sade.eperusteet.ylops.service.mapping.DtoMapper;
@@ -34,6 +37,9 @@ public class NavigationBuilderLops2019Impl implements NavigationBuilder {
 
     @Autowired
     private EperusteetService eperusteetService;
+
+    @Autowired
+    private Lops2019OpintojaksoService opintojaksoService;
 
     @Autowired
     private Lops2019SisaltoRepository lops2019SisaltoRepository;
@@ -63,64 +69,40 @@ public class NavigationBuilderLops2019Impl implements NavigationBuilder {
     }
 
     private NavigationNodeDto oppiaineet(Long opsId) {
-        final Lops2019Sisalto sisalto = lops2019SisaltoRepository.findOne(opsId);
-
         List<Lops2019OppiaineKaikkiDto> oppiaineet = lopsService.getPerusteOppiaineet(opsId);
         List<Lops2019PaikallinenOppiaineDto> paikallisetOppiaineet = oppiaineService.getAll(opsId);
 
-        List<Lops2019OppiaineKaikkiDto> kaikki = new ArrayList<>();
-        kaikki.addAll(oppiaineet);
-        //kaikki.addAll(oppimaarat);
-
+        Map<String, Set<Lops2019OpintojaksoDto>> opintojaksotMap = new HashMap<>();
+        List<Lops2019OpintojaksoDto> opsOpintojaksot = opintojaksoService.getAll(opsId);
+        if (!ObjectUtils.isEmpty(opsOpintojaksot)) {
+            opsOpintojaksot.forEach(oj -> {
+                Set<Lops2019OpintojaksonOppiaineDto> ojOppiaineet = oj.getOppiaineet();
+                if (!ObjectUtils.isEmpty(ojOppiaineet)) {
+                    ojOppiaineet.forEach(oa -> {
+                        String key = oa.getKoodi();
+                        if (!ObjectUtils.isEmpty(key)) {
+                            if (!opintojaksotMap.containsKey(key)) {
+                                opintojaksotMap.put(key, new HashSet<>());
+                            }
+                            opintojaksotMap.get(key).add(oj);
+                        }
+                    });
+                }
+            });
+        }
 
         return NavigationNodeDto.of(NavigationType.oppiaineet)
                 .addAll(oppiaineet.stream()
-                        .map(oa -> mapOppiaine(oa))
+                        .map(oa -> mapOppiaine(oa, opintojaksotMap))
                         .collect(Collectors.toList()))
                 .addAll(paikallisetOppiaineet.stream()
-                        .map(poa -> mapPaikallinenOppiaine(poa))
+                        .map(this::mapPaikallinenOppiaine)
                         .collect(Collectors.toList()));
-
-        /*
-        List<Lops2019Oppiaine> oppiaineet = sisalto.getOppiaineet();
-        List<Lops2019Oppiaine> oppimaarat = new ArrayList<>();
-        if (!ObjectUtils.isEmpty(oppiaineet)) {
-            oppimaarat = lops2019OppiaineRepository.getOppimaaratByParents(oppiaineet);
-        }
-
-        List<Lops2019Oppiaine> kaikki = new ArrayList<>();
-        kaikki.addAll(oppiaineet);
-        kaikki.addAll(oppimaarat);
-
-        Map<Lops2019Oppiaine, List<Lops2019Oppiaine>> oppimaaratMap = new HashMap<>();
-        oppimaarat.forEach(om -> {
-            if (!oppimaaratMap.containsKey(om.getOppiaine())) {
-                oppimaaratMap.put(om.getOppiaine(), new ArrayList<>());
-            }
-            oppimaaratMap.get(om.getOppiaine()).add(om);
-        });
-
-        Map<Lops2019Oppiaine, List<Lops2019Moduuli>> moduulitMap = new HashMap<>();
-        List<Lops2019Moduuli> moduulit = new ArrayList<>();
-        if (!ObjectUtils.isEmpty(kaikki)) {
-            moduulit = lops2019ModuuliRepository.getModuulitByParents(kaikki);
-        }
-        moduulit.forEach(m -> {
-            if (!moduulitMap.containsKey(m.getOppiaine())) {
-                moduulitMap.put(m.getOppiaine(), new ArrayList<>());
-            }
-            moduulitMap.get(m.getOppiaine()).add(m);
-        });
-
-        return NavigationNodeDto.of(NavigationType.oppiaineet)
-                .addAll(oppiaineet.stream()
-                        .map(oa -> mapOppiaine(oa, oppimaaratMap, moduulitMap))
-                        .collect(Collectors.toList()));
-        */
     }
 
     private NavigationNodeDto mapOppiaine(
-            Lops2019OppiaineKaikkiDto oa
+            Lops2019OppiaineKaikkiDto oa,
+            Map<String, Set<Lops2019OpintojaksoDto>> opintojaksotMap
     ) {
         NavigationNodeDto result = NavigationNodeDto
                 .of(NavigationType.oppiaine, mapper.map(oa.getNimi(), LokalisoituTekstiDto.class), oa.getId())
@@ -128,7 +110,32 @@ public class NavigationBuilderLops2019Impl implements NavigationBuilder {
 
         if (!ObjectUtils.isEmpty(oa.getOppimaarat())) {
             result.add(NavigationNodeDto.of(NavigationType.oppimaarat)
-                    .addAll(oa.getOppimaarat().stream().map(this::mapOppiaine)));
+                    .addAll(oa.getOppimaarat().stream().map(om -> mapOppiaine(om, opintojaksotMap))));
+        }
+
+        if (oa.getKoodi() != null && oa.getKoodi().getUri() != null
+                && opintojaksotMap.containsKey(oa.getKoodi().getUri())
+                && !ObjectUtils.isEmpty(opintojaksotMap.get(oa.getKoodi().getUri()))) {
+            Set<Lops2019OpintojaksoDto> oaOpintojaksot = opintojaksotMap.get(oa.getKoodi().getUri());
+            result.add(NavigationNodeDto.of(NavigationType.opintojaksot)
+                    .addAll(oaOpintojaksot.stream()
+                            .map(ojOa -> NavigationNodeDto.of(
+                                    NavigationType.opintojakso,
+                                    mapper.map(ojOa.getNimi(), LokalisoituTekstiDto.class),
+                                    ojOa.getId())
+                                    .meta("koodi", ojOa.getKoodi()))));
+        }
+
+        List<Lops2019ModuuliDto> moduulit = oa.getModuulit();
+        if (!ObjectUtils.isEmpty(moduulit)) {
+            result.add(NavigationNodeDto.of(NavigationType.moduulit)
+                    .addAll(moduulit.stream()
+                            .map(m -> NavigationNodeDto.of(
+                                    NavigationType.moduuli,
+                                    mapper.map(m.getNimi(), LokalisoituTekstiDto.class),
+                                    m.getId())
+                                    .meta("koodi", mapper.map(m.getKoodi(), KoodiDto.class))
+                                    .meta("pakollinen", m.isPakollinen()))));
         }
 
         return result;
